99bc611b7ea132c4ef72b58533fe52a8266cf435
meenzoon <rlaalswns523@gmail.com>
2023-09-13 23:55:45 +0900

    [ADD] local 모드 실행 코드 추가

--- src/main/java/org/meenzoon/spark/CsvToParquet.java
+++ src/main/java/org/meenzoon/spark/CsvToParquet.java
@@ -3,16 +3,20 @@ package org.meenzoon.spark;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.col;
 
 public class CsvToParquet {
   public static void main(String[] args) {
     SparkSession spark = SparkSession
         .builder()
         .appName("Java Spark SQL")
+        .master("local")
+        .config("spark.driver.bindAddress", "127.0.0.1")
         .getOrCreate();
 
-    Dataset<Row> df = spark.read().csv("path/input/file.csv").toDF();
+    Dataset<Row> df = spark.read().csv("file.csv").toDF();
+    df.filter(col("g").gt(37.5)).write().parquet("file.parquet");
 
-    df.write().parquet("path/output/file.parquet");
+    spark.stop();
   }
 }
