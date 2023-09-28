b7825f0eab6750c238aedf7766613114d212a0ad
meenzoon <rlaalswns523@gmail.com>
2023-09-28 23:00:22 +0900

    [MOD] function 활용

--- src/main/scala/org/meenzoon/spark/NycTaxi.scala
+++ src/main/scala/org/meenzoon/spark/NycTaxi.scala
@@ -1,6 +1,7 @@
 package org.meenzoon.spark
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.sum
 
 object NycTaxi {
   def main(args: Array[String]): Unit = {
@@ -10,8 +11,11 @@ object NycTaxi {
       .master("local")
       .config("spark.driver.bindAddress", "127.0.0.1")
       .getOrCreate()
+    import sparkSession.implicits._
 
     val df = sparkSession.read.parquet("/Users/meenzoon/workspace/data/nyc-taxi/*.parquet")
+
+    df.select(sum($"total_amount")).show()
 
     df.show()
     df.printSchema()
