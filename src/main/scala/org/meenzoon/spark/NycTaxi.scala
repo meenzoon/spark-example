d60ce7c351c74a9a7588fd4934e2f74b3651b1c1
meenzoon <rlaalswns523@gmail.com>
2023-09-30 15:45:25 +0900

    map, reduce 활용 방법 확인

--- src/main/scala/org/meenzoon/spark/NycTaxi.scala
+++ src/main/scala/org/meenzoon/spark/NycTaxi.scala
@@ -1,7 +1,7 @@
 package org.meenzoon.spark
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.sum
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions.{max, sum}
 
 object NycTaxi {
   def main(args: Array[String]): Unit = {
@@ -15,10 +15,30 @@ object NycTaxi {
 
     val df = sparkSession.read.parquet("/Users/meenzoon/workspace/data/nyc-taxi/*.parquet")
 
-    df.select(sum($"total_amount")).show()
+    //df.select(sum($"total_amount")).show()
 
-    df.show()
-    df.printSchema()
+    val df2 = df.rdd.map(s => s.getInt(0))
+
+    df2.toDS().show()
+    df2.toDS().printSchema()
+
+    val df3 = df2.reduce((x, y) => x + y)
+    /*
+    df2.toDS().write.mode(SaveMode.Overwrite)
+      .option("compression", "gzip")
+      .csv("/Users/meenzoon/workspace/data/output/")
+    */
+
+
+
+    //val df3 = df2.reduce((x, y) => x + y)
+      //.reduceByKey((x, y) => x + y).toDS()
+
+    //df3.show()
+    //df3.printSchema()
+
+    //df.show()
+    //df.printSchema()
 
     sparkSession.stop()
   }
