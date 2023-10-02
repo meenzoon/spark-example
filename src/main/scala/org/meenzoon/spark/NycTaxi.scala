127ee6d28ab2b12f60a6b7bc0c24e1d2ef1247b6
meenzoon <rlaalswns523@gmail.com>
2023-10-02 23:32:46 +0900

    [MOD] 여러 필드 map 방법

--- src/main/scala/org/meenzoon/spark/NycTaxi.scala
+++ src/main/scala/org/meenzoon/spark/NycTaxi.scala
@@ -17,19 +17,27 @@ object NycTaxi {
 
     //df.select(sum($"total_amount")).show()
 
-    val df2 = df.rdd.map(s => s.getInt(0))
+    val df2 = df.rdd.map(line => {
+      val field1 = line(3)
+      val field2 = line(4)
 
-    df2.toDS().show()
-    df2.toDS().printSchema()
+      val transformedField1 = field1.toString
+      val transformedField2 = field2.toString
+      // 변환된 필드들을 다시 콤마로 연결하여 반환
+      s"$transformedField1,$transformedField2"
+    })
 
-    val df3 = df2.reduce((x, y) => x + y)
+    df2.foreach(println)
+
+    //df2.toDS().show()
+    //df2.toDS().printSchema()
+
+    //val df3 = df2.reduce((x, y) => x + y)
     /*
     df2.toDS().write.mode(SaveMode.Overwrite)
       .option("compression", "gzip")
       .csv("/Users/meenzoon/workspace/data/output/")
     */
-
-
 
     //val df3 = df2.reduce((x, y) => x + y)
       //.reduceByKey((x, y) => x + y).toDS()
@@ -37,8 +45,8 @@ object NycTaxi {
     //df3.show()
     //df3.printSchema()
 
-    //df.show()
-    //df.printSchema()
+    df.show()
+    df.printSchema()
 
     sparkSession.stop()
   }
